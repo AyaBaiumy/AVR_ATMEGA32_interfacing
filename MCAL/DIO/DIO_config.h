f5e04e6a7b313b2539da16f2e16d3e2c64910373
AyaBaiumy <140268169+AyaBaiumy@users.noreply.github.com>
2023-07-22 21:12:52 +0300

    Create DIO_config.h

---
+++ MCAL/DIO/DIO_config.h
@@ -0,0 +1,55 @@
+/*
+ * DIO_includes.h
+ *
+ *  Created on: Jul 22, 2023
+ *      Author: AyaBaiumy
+ */
+
+#ifndef DRIVERS_MCAL_DIO_DIO_CONFIG_H_
+#define DRIVERS_MCAL_DIO_DIO_CONFIG_H_
+
+
+#define DIO_u8_PINA_DIR_0    DIO_u8_INPUT
+#define DIO_u8_PINA_DIR_1    DIO_u8_INPUT
+#define DIO_u8_PINA_DIR_2    DIO_u8_INPUT
+#define DIO_u8_PINA_DIR_3    DIO_u8_INPUT
+#define DIO_u8_PINA_DIR_4    DIO_u8_INPUT
+#define DIO_u8_PINA_DIR_5    DIO_u8_INPUT
+#define DIO_u8_PINA_DIR_6    DIO_u8_INPUT
+#define DIO_u8_PINA_DIR_7    DIO_u8_INPUT
+
+
+
+#define DIO_u8_PINB_DIR_0    DIO_u8_INPUT
+#define DIO_u8_PINB_DIR_1    DIO_u8_INPUT
+#define DIO_u8_PINB_DIR_2    DIO_u8_INPUT
+#define DIO_u8_PINB_DIR_3    DIO_u8_INPUT
+#define DIO_u8_PINB_DIR_4    DIO_u8_INPUT
+#define DIO_u8_PINB_DIR_5    DIO_u8_INPUT
+#define DIO_u8_PINB_DIR_6    DIO_u8_INPUT
+#define DIO_u8_PINB_DIR_7    DIO_u8_INPUT
+
+
+
+#define DIO_u8_PINC_DIR_0    DIO_u8_INPUT
+#define DIO_u8_PINC_DIR_1    DIO_u8_INPUT
+#define DIO_u8_PINC_DIR_2    DIO_u8_INPUT
+#define DIO_u8_PINC_DIR_3    DIO_u8_INPUT
+#define DIO_u8_PINC_DIR_4    DIO_u8_INPUT
+#define DIO_u8_PINC_DIR_5    DIO_u8_INPUT
+#define DIO_u8_PINC_DIR_6    DIO_u8_INPUT
+#define DIO_u8_PINC_DIR_7    DIO_u8_INPUT
+
+
+
+#define DIO_u8_PIND_DIR_0    DIO_u8_INPUT
+#define DIO_u8_PIND_DIR_1    DIO_u8_INPUT
+#define DIO_u8_PIND_DIR_2    DIO_u8_INPUT
+#define DIO_u8_PIND_DIR_3    DIO_u8_INPUT
+#define DIO_u8_PIND_DIR_4    DIO_u8_INPUT
+#define DIO_u8_PIND_DIR_5    DIO_u8_INPUT
+#define DIO_u8_PIND_DIR_6    DIO_u8_INPUT
+#define DIO_u8_PIND_DIR_7    DIO_u8_INPUT
+
+#endif /* DRIVERS_MCAL_DIO_DIO_CONFIG_H_ */
+
