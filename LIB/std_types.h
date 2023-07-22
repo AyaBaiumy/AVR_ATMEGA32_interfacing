69053c46ea5935e5ac3bd4f364988ad7db0cecfc
AyaBaiumy <140268169+AyaBaiumy@users.noreply.github.com>
2023-07-22 21:21:16 +0300

    Create std_types.h

---
+++ LIB/std_types.h
@@ -0,0 +1,26 @@
+/*
+ * DIO_program.c
+ *
+ *  Created on: Jul 22, 2023
+ *      Author: AyaBaiumy
+ */
+
+
+#ifndef _STD_types_H_
+#define _STD_types_H_
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long int u32;
+typedef signed char s8;
+typedef signed short int s16;
+typedef signed long int s32;
+typedef float f32;
+typedef double f64;
+typedef long double f96;
+
+
+typedef void (*pf)(void);
+
+#endif
+
