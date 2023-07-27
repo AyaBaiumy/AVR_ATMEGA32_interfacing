22b988ac15bde6a5bb3adb3b448ec0a8cd0aa4be
AyaBaiumy <140268169+AyaBaiumy@users.noreply.github.com>
2023-07-28 00:05:10 +0300

    Create KEYPAD_Int.h

---
+++ HAL/KEYPAD/KEYPAD_Int.h
@@ -0,0 +1,16 @@
+/*
+ * KEYPAD_interface.h
+ *
+ *  Created on: Jul 22, 2023
+ *      Author: AyaBaiumy
+ */
+
+#ifndef DRIVERS_HAL_KEYPAD_KEYPAD_INTERFACE_H_
+#define DRIVERS_HAL_KEYPAD_KEYPAD_INTERFACE_H_
+
+void KEY_PAD_Initialization();
+
+u8 KEY_PAD_u8GetPressedKey(void);
+
+#endif /* DRIVERS_HAL_KEYPAD_KEYPAD_INTERFACE_H_ */
+
